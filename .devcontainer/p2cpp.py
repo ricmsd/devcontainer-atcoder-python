6938238d2b3f95babaf4e292fd865e690f5aba32
ricmsd <ricmsd@gmail.com>
2023-03-30 09:52:04 +0900

    .py file formatted in black formatter.

--- .devcontainer/p2cpp.py
+++ .devcontainer/p2cpp.py
@@ -7,11 +7,12 @@ import openai
 # - Improve log output
 # - Error handling
 
+
 def chat_completion(model: str, python_code: str) -> str:
     """
-    chat completion for gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301	
+    chat completion for gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301
     """
-    print(f'[INFO] call openai API and wait response: model={model}')
+    print(f"[INFO] call openai API and wait response: model={model}")
     prompt = f"Please convert the following Python code to C++ code:\n\n{python_code}\n\nC++ code:\n"
     response = openai.ChatCompletion.create(
         model=model,
@@ -21,13 +22,14 @@ def chat_completion(model: str, python_code: str) -> str:
         ],
         max_tokens=2000,
     )
-    return response.choices[0].message['content'].strip()
+    return response.choices[0].message["content"].strip()
+
 
 def completion(model: str, python_code: str) -> str:
     """
     completion for text-davinci-003, text-davinci-002, text-curie-001, text-babbage-001, text-ada-001, davinci, curie, babbage, ada
     """
-    print(f'[INFO] call openai API and wait response: model={model}')
+    print(f"[INFO] call openai API and wait response: model={model}")
     prompt = f"Please convert the following Python code to C++:\n\n{python_code}\n"
     response = openai.Completion.create(
         model=model,
@@ -39,31 +41,52 @@ def completion(model: str, python_code: str) -> str:
     )
     return response.choices[0].text.strip()
 
+
 def convert_python_to_cpp(model: str, python_code: str) -> str:
-    if model in ["gpt-4", "gpt-4-0314", "gpt-4-32k", "gpt-4-32k-0314", "gpt-3.5-turbo", "gpt-3.5-turbo-0301"]:
+    if model in [
+        "gpt-4",
+        "gpt-4-0314",
+        "gpt-4-32k",
+        "gpt-4-32k-0314",
+        "gpt-3.5-turbo",
+        "gpt-3.5-turbo-0301",
+    ]:
         return chat_completion(model, python_code)
-    elif model in ["text-davinci-003", "text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001", "davinci", "curie", "babbage", "ada"]:
+    elif model in [
+        "text-davinci-003",
+        "text-davinci-002",
+        "text-curie-001",
+        "text-babbage-001",
+        "text-ada-001",
+        "davinci",
+        "curie",
+        "babbage",
+        "ada",
+    ]:
         return completion(model, python_code)
     raise Exception(f"unknown model: {model}")
+
 
 model = sys.argv[1]
 input_py = sys.argv[2]
 output_cpp = sys.argv[3]
 
-openai.api_key = os.environ['OPENAI_API_KEY']
-print(f'[INFO] read python file: {input_py}')
+openai.api_key = os.environ["OPENAI_API_KEY"]
+print(f"[INFO] read python file: {input_py}")
 with open(input_py, "r") as file:
     python_code = file.read()
 
 cpp_code = convert_python_to_cpp(model, python_code)
-print(f'[INFO] write C++ file: {output_cpp}')
+print(f"[INFO] write C++ file: {output_cpp}")
 with open(output_cpp, "w") as file:
     file.write(cpp_code)
 
-print(f'[INFO] compile {output_cpp} by g++')
-result = subprocess.run(f"g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -o ./a.out {output_cpp}".split())
+print(f"[INFO] compile {output_cpp} by g++")
+result = subprocess.run(
+    f"g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -o ./a.out {output_cpp}".split()
+)
 if result.returncode != 0:
-    print(f'[ERROR] compile failed: g++ exit status={result.returncode}')
+    print(f"[ERROR] compile failed: g++ exit status={result.returncode}")
     exit(1)
 
-print(f'[INFO] output execution file: a.out')
+print(f"[INFO] output execution file: a.out")
